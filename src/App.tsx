d2a8e3911e77e9f97e0c3bc33559faa09b7f7204
Justin Gnananadchtheram <justinedward8@gmail.com>
2026-01-11 11:23:47 -0500

    configured shader gradient component

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,38 @@ function App() {
     <div>
       <ShaderGradientCanvas style={{ position: "absolute", pointerEvents: "none" }}>
         <ShaderGradient
-          color1='#ff5005'
-          color2='#dbba95'
-          color3='#d0bce1'
-          positionX={0}
-          positionY={0}
-          grain='off'
-        />
+            animate="on"
+            brightness={1}
+            cAzimuthAngle={180}
+            cDistance={3.9}
+            cPolarAngle={115}
+            cameraZoom={1}
+            color1="#ffaf8a"
+            color2="#ec2651"
+            color3="#000000"
+            envPreset="city"
+            grain="on"
+            lightType="3d"
+            positionX={-0.7}
+            positionY={-0.1}
+            positionZ={0}
+            range="disabled"
+            rangeEnd={40}
+            rangeStart={0}
+            reflection={0.1}
+            rotationX={0}
+            rotationY={0}
+            rotationZ={235}
+            shader="defaults"
+            type="waterPlane"
+            uAmplitude={0}
+            uDensity={2.2}
+            uFrequency={5.5}
+            uSpeed={0.1}
+            uStrength={1.2}
+            uTime={0.2}
+            wireframe={false}
+            />
       </ShaderGradientCanvas>
     </div>
   )
